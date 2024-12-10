aee9ff3725e18ab6364d08085c0e8d0efa999171
yeongenn <yngn0012@naver.com>
2024-12-10 23:58:13 +0900

    UserController 주석 추가

--- kopring-study/src/main/kotlin/com/yeongenn/kopringstudy/kopring/controller/UserController.kt
+++ kopring-study/src/main/kotlin/com/yeongenn/kopringstudy/kopring/controller/UserController.kt
@@ -15,30 +15,58 @@ class UserController(
     private val userService: UserService,
 ) {
 
+    /*
+    사용자 등록하기
+    [POST] /user
+    @param UserCreateRequest
+    @return ResponseEntity<CommonResponseDto<UserCreateRequest>>
+     */
     @PostMapping("/user")
     fun addUser(@RequestBody req: UserCreateRequest): ResponseEntity<CommonResponseDto<UserCreateRequest>> {
         userService.addUser(req)
         return ResponseEntity.ok(CommonResponseDto(true, req))
     }
 
+    /*
+    전체 사용자 조회하기
+    [GET] /user
+    @return ResponseEntity<CommonResponseDto<List<UserResponse>>>
+     */
     @GetMapping("/user")
     fun getUsers(): ResponseEntity<CommonResponseDto<List<UserResponse>>> {
         val users: List<UserResponse> = userService.getUsers()
         return ResponseEntity.ok(CommonResponseDto(true, users))
     }
 
+    /*
+    사용자 정보 수정하기
+    [PUT] /user
+    @param UserUpdateRequest
+    @return ResponseEntity<CommonResponseDto<UserUpdateRequest>>
+     */
     @PutMapping("/user")
     fun updateUserName(@RequestBody req: UserUpdateRequest): ResponseEntity<CommonResponseDto<UserUpdateRequest>> {
         userService.updateUser(req)
         return ResponseEntity.ok(CommonResponseDto(true, req))
     }
 
+    /*
+    사용자 삭제하기
+    [DELETE] /user
+    @param String
+    @return ResponseEntity<CommonResponseDto<String>>
+     */
     @DeleteMapping("/user")
     fun deleteUser(userId: String): ResponseEntity<CommonResponseDto<String>> {
         userService.deleteUser(userId)
         return ResponseEntity.ok(CommonResponseDto(true, "${userId}님 삭제 완료"))
     }
 
+    /*
+    대출 현황 조회하기
+    [GET] /user/loan
+    @return ResponseEntity<CommonResponseDto<List<UserLoanHistoryResponse>>>
+     */
     @GetMapping("/user/loan")
     fun getUserLoanHistories(): ResponseEntity<CommonResponseDto<List<UserLoanHistoryResponse>>> {
         val userLoanHistories: List<UserLoanHistoryResponse> = userService.getUserLoanHistories()
